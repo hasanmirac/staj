564fad374266c2c9e56784351affc60da5b2d065
hasanmirac <61428133+hasanmirac@users.noreply.github.com>
2021-07-06 22:46:18 +0300

    proje-baslangıc

---
+++ app.js
@@ -0,0 +1,15 @@
+// Elementleri Seçme
+const projectForm = document.querySelector("#project-form");
+const projectList = document.querySelector(".list-group");
+
+eventListener();
+
+function eventListener() { // Tüm event listenerlar
+    projectForm.addEventListener("submit",addProject);
+    
+}
+
+
+function addProject(e) {
+    
+}
