8d7adb841b96a801f6bf89186ad9ca3efa2b3991
hasanmirac <61428133+hasanmirac@users.noreply.github.com>
2021-07-10 11:42:41 +0300

    Proje sayfası js özellikleri eklendi.

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
 // Elementleri Seçme
 const projectForm = document.querySelector("#project-form");
 const projectList = document.querySelector(".list-group");
-const projectButton = document.querySelector("#new-project");
-
+const newProjectButton = document.querySelector(".btn-success");
+const closeButton = document.querySelector(".cancel");
+const deleteButton = document.querySelector("#delete-project");
+const popup = document.querySelector(".popup");
+const inputProject = document.querySelector("#project");
 
 eventListener();
 
+
 function eventListener() { // Tüm event listenerlar
-    projectButton.addEventListener("click",newProject);
-    
-}   
+    newProjectButton.addEventListener("click",openForm);
+    closeButton.addEventListener("click",closeForm);
+    projectForm.addEventListener("submit",addProject);
 
-
-function newProject(e) {
-    document.querySelector(".popup").style.display = "flex";
 }
+
+function closeForm() {
+    popup.style.display = "none";
+}
+function openForm() {
+    popup.style.display = "block";
+}
+
+function addProject(e) {
+    const newProject = inputProject.value.trim();
+    
+    addProjectToUI(newProject); // Değeri arayüze gönderme
+
+
+    e.preventDefault();
+}
+
+function addProjectToUI(newProject) { // String değerini list item olarak UI'ya ekler
+    // List item oluşturma
+    const listItem = document.createElement("li");
+    // Link oluşturma
+    const link = document.createElement("a");
+    link.href = "#";  
+    link.className = "edit-item";
+    link.innerHTML = "<i class = 'fa fa-edit'></i>";
+
+    listItem.className = "list-group-item d-flex justify-content-between";
+
+    // Text Node Ekleme
+    listItem.appendChild(document.createTextNode(newProject));
+    listItem.appendChild(link);
+
+    //- Todo List'e List Item'ı ekleme
+    projectList.appendChild(listItem);
+    inputProject.value = "";
+    popup.style.display = "none";
+}
+
+
