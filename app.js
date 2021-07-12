e35168e4419801805382360bb50ba26d0766b7df
hasanmirac <61428133+hasanmirac@users.noreply.github.com>
2021-07-12 11:20:25 +0300

    Projeler sayfası büyük oranı tamamlandı

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Elementleri Seçme
 const projectForm = document.querySelector("#project-form");
 const projectList = document.querySelector(".list-group");
-const newProjectButton = document.querySelector(".btn-success");
+const newProjectButton = document.querySelector("#new-project");
 const closeButton = document.querySelector(".cancel");
 const deleteButton = document.querySelector("#delete-project");
 const popup = document.querySelector(".popup");
+const popupSave = document.querySelector
 const inputProject = document.querySelector("#project");
+const homePopup = document.querySelector("#home-popup");
+const cardBody = document.querySelector(".card-body");
+
 
 eventListener();
 
@@ -14,6 +18,8 @@ function eventListener() { // Tüm event listenerlar
     newProjectButton.addEventListener("click",openForm);
     closeButton.addEventListener("click",closeForm);
     projectForm.addEventListener("submit",addProject);
+    document.addEventListener("DOMContentLoaded",loadAllProjetsToUI); // Sayfa yüklendiğinde çalışacak
+    cardBody.addEventListener("click",deleteProject);
 
 }
 
@@ -24,14 +30,106 @@ function openForm() {
     popup.style.display = "block";
 }
 
+function deleteProject(e) {
+
+    if (e.target.className === "fa fa-remove") {
+        
+        e.target.parentElement.parentElement.remove();
+        deleteProjectFromStorage(e.target.parentElement.parentElement.textContent);
+        showAlertHome("success","Silme işlemi başarılı")
+    }
+    
+}
+
+function deleteProjectFromStorage(deleteProject) { //Projeleri Storagedan silme
+    let projects = getProjetsFromStorage();
+
+    projects.forEach(function(project,index) {
+        if (project === deleteProject) {
+            projects.splice(index,1); // Arrayden değeri siler
+        }
+        
+        localStorage.setItem("projets",JSON.stringify(projects));
+    });
+
+}
+
+function loadAllProjetsToUI() {
+    let projects = getProjetsFromStorage();
+
+    projects.forEach(function(project) {
+        addProjectToUI(project);
+    });
+}
+
 function addProject(e) {
     const newProject = inputProject.value.trim();
     
-    addProjectToUI(newProject); // Değeri arayüze gönderme
-
-
+    if(newProject === "") {
+        showAlert("danger","Lütfen bir proje giriniz...");
+    } 
+    else {
+        addProjectToUI(newProject); // Değeri arayüze gönderme
+        addProjectToStorage(newProject); // Değeri storage'a gönderme
+        showAlert("success","Proje başarıyla eklendi...");
+        setTimeout(() => {
+            popup.style.display = "none";
+        }, 1000); 
+    }
     e.preventDefault();
 }
+
+function getProjetsFromStorage() { // Storage'dan tüm projeleri alma
+    let projects;
+
+    if (localStorage.getItem("projets") === null) {
+        projects = [];
+    }
+    else {
+        projects = JSON.parse(localStorage.getItem("projets"));
+    }
+
+    return projects;
+}
+
+function addProjectToStorage(newProject) {
+    let projects = getProjetsFromStorage();
+
+    projects.push(newProject);
+
+    localStorage.setItem("projets",JSON.stringify(projects));
+
+}
+
+function showAlert(type,message) { //popup'da görünecek
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.textContent = message;
+
+    homePopup.appendChild(alert); //popup'da görünecek
+    
+
+    setTimeout(function(){ // belirli süre görünüp kaybolacak
+        alert.remove();
+    },1000); 
+    
+}
+
+function showAlertHome(type,message) { //home'da görünecek
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.textContent = message;
+
+    
+    cardBody.appendChild(alert); // anasayfa'da görünecek
+
+    setTimeout(function(){ // belirli süre görünüp kaybolacak
+        alert.remove();
+    },1000); 
+    
+}
+
+
 
 function addProjectToUI(newProject) { // String değerini list item olarak UI'ya ekler
     // List item oluşturma
@@ -40,18 +138,23 @@ function addProjectToUI(newProject) { // String değerini list item olarak UI'ya
     const link = document.createElement("a");
     link.href = "#";  
     link.className = "edit-item";
-    link.innerHTML = "<i class = 'fa fa-edit'></i>";
+    link.innerHTML = "<i class = 'fa fa-edit' 'fa fa-remove'></i>";
+    const link2 = document.createElement("a");
+    link2.href = "#";  
+    link2.className = "remove-item";
+    link2.innerHTML = "<i class ='fa fa-remove'></i>";
+
 
     listItem.className = "list-group-item d-flex justify-content-between";
 
     // Text Node Ekleme
     listItem.appendChild(document.createTextNode(newProject));
     listItem.appendChild(link);
+    listItem.appendChild(link2);
 
     //- Todo List'e List Item'ı ekleme
     projectList.appendChild(listItem);
     inputProject.value = "";
-    popup.style.display = "none";
+    //popup.style.display = "none";
 }
-
 
