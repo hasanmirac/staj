c92872531df94ce8ded3d0319348b83a76bca618
hasanmirac <61428133+hasanmirac@users.noreply.github.com>
2021-08-03 23:33:30 +0300

    2 tema tamamlandı

--- task.js
+++ task.js
@@ -72,36 +72,36 @@ const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
-  document.body.classList.add('darkmode');
+    document.body.classList.add('darkmode');
   // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
+    localStorage.setItem('darkMode', 'enabled');
 }
 
 const disableDarkMode = () => {
   // 1. Remove the class from the body
-  document.body.classList.remove('darkmode');
+    document.body.classList.remove('darkmode');
   // 2. Update darkMode in localStorage 
-  localStorage.setItem('darkMode', null);
+    localStorage.setItem('darkMode', null);
 }
- 
+
 // If the user already visited and enabled darkMode
 // start things off with it on
 if (darkMode === 'enabled') {
-  enableDarkMode();
+    enableDarkMode();
 }
 
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
   // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode'); 
-  
+    darkMode = localStorage.getItem('darkMode'); 
+
   // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
+    if (darkMode !== 'enabled') {
     enableDarkMode();
   // if it has been enabled, turn it off  
-  } else {  
+    } else {  
     disableDarkMode(); 
-  }
+    }
 });
 
 
